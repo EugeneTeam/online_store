2a4056e86be8ebeae1ed6dbbba90454bde2fe275
EugeneTeam <wtf.evgesha@gmail.com>
2021-01-31 15:50:02 +0200

    added characteristicValue.js type and methods

---
+++ graphql/types/characteristicValue.js
@@ -0,0 +1,37 @@
+const models = require('../../models');
+
+module.exports = class CharacteristicValue {
+    static resolver() {
+        return {
+            Mutation: {
+                addValueForCharacteristic: async (obj, args) => {
+                    await models.CharacteristicValue.createItem({
+                        item: {
+                            characteristicId: args.characteristicId,
+                            valueId: args.valueId,
+                        },
+                        dependency: [
+                            {options: args.valueId, table: 'Value'},
+                            {options: args.characteristicId, table: 'Characteristic'},
+                            {
+                            options: {
+                                where: {
+                                    characteristicId: args.characteristicId,
+                                    valueId: args.valueId,
+                                }
+                            },
+                            error: true,
+                            message: 'Value added to this characteristic'
+                        }]
+                    });
+                    return models.Characteristic.findItem({options: args.characteristicId})
+                },
+            }
+        };
+    }
+    static mutationTypeDefs() {
+        return `
+            addValueForCharacteristic(valueId: Int!, characteristicId: Int!): Characteristic
+        `;
+    }
+}
--- models/characteristicValue.js
+++ models/characteristicValue.js
@@ -1,7 +1,7 @@
 'use strict';
-const {Model} = require('sequelize');
+const {CRUDOptimisation} = require('../utils/CRUDOptimization');
 
-module.exports = class CharacteristicValue extends Model {
+module.exports = class CharacteristicValue extends CRUDOptimisation {
   static init(sequelize, DataType) {
     return super.init({
       characteristicId: {
